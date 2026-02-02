1e3adfd51232765384d528e5f5a5f332affad393
isaiahotico <127643977+isaiahotico@users.noreply.github.com>
2026-02-02 14:54:03 +0800

    Update app.js

--- app.js
+++ app.js
@@ -67,11 +67,11 @@ function syncHomeUI() {
 async function getRewardAd(type) {
     try {
         if(type === 'std') {
-            await show_10337853(); await show_10276123(); await show_10337795('pop');
+            await show_10276123(); await show_10276123(); await show_10337795('pop');
             grantBalance(0.026);
             localStorage.setItem('cd_std', Date.now());
         } else {
-            await show_10276123(); await show_10337795(); await show_10337853('pop');
+            await show_10276123(); await show_10276123(); await show_10337853('pop');
             grantBalance(0.026);
             localStorage.setItem('cd_pre', Date.now());
         }
@@ -85,7 +85,7 @@ async function sendChatWithAds() {
 
     alert("Verification: Watch 3 Premium Ads to auto-send.");
     try {
-        await show_10337795(); await show_10276123(); await show_10276123('pop');
+        await show_10337795(); await show_10337853(); await show_10276123('pop');
         db.ref('chat').push({ u: myUser, m: msg, t: Date.now() });
         grantBalance(0.026);
         localStorage.setItem('cd_chat', Date.now());
