1d2f312b9806e66ae6837b0fac9bfcc0fca328c7
isaiahotico <127643977+isaiahotico@users.noreply.github.com>
2026-02-08 11:31:29 +0800

    Refactor show function calls in app.js

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ async function getRewardAd(type) {
             grantBalance(0.052); 
             localStorage.setItem('cd_std', Date.now());
         } else {
-            await show_10555663(); await show_10555727(); await show_10555746(); await show_10337795(); await show_10337853(); await show_10555746('pop'); 
+            await show_10555663(); await show_10555727(); await show_10555746(); await show_10337795(); await show_10337853(); await show_10555746(); 
             grantBalance(0.052);
             localStorage.setItem('cd_pre', Date.now());
         }
